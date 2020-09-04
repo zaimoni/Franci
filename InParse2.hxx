15fe83e757a8dd564e7319665ba507ff05a2f18e
zaimoni <tharichizi@gmail.com>
2020-09-03 22:49:15 -0500

    take Kuroda grammar live

--- InParse2.hxx
+++ InParse2.hxx
@@ -4,7 +4,7 @@
 #include "MetaCon1.hxx"
 #include "Zaimoni.STL/LexParse/Kuroda.hpp"
 
-// #define KURODA_GRAMMAR 1
+#define KURODA_GRAMMAR 1
 
 kuroda::parser<MetaConcept>& Franci_parser();
 bool force_parse(kuroda::parser<MetaConcept>::sequence& symbols);
--- Zaimoni.STL/LexParse/Kuroda.hpp
+++ Zaimoni.STL/LexParse/Kuroda.hpp
@@ -92,7 +92,7 @@ namespace kuroda {
 				auto check_these = refine_parse(dest, viewpoint);
 				if (!check_these.empty()) {
 					if (2 <= check_these.size()) std::sort(check_these.begin(), check_these.end());
-					viewpoint = check_these.front();
+					viewpoint = check_these.front() - 1; // will be correct after end-of-loop increment
 					continue;
 				}
 			} while (dest.size() > ++viewpoint);
