c5778fa4fb6d3f3ff7475e70dbe335bf3e6a81a6
zaimoni <tharichizi@gmail.com>
2022-08-16 05:58:51 -0500

    checkpoint lex_node class

--- Gentzen.cpp
+++ Gentzen.cpp
@@ -162,6 +162,143 @@ static auto to_lines(std::istream& in, formal::src_location& origin)
 	return ret;
 }
 
+// following class will need extraction once it is stable
+class lex_node
+{
+	using lexeme = std::variant<formal::word, lex_node>;
+
+private:
+	kuroda::parser<lexeme>::symbols _prefix;
+	kuroda::parser<lexeme>::symbols _infix;
+	kuroda::parser<lexeme>::symbols _postfix;
+	std::unique_ptr<lexeme> _anchor;
+	std::unique_ptr<lexeme> _post_anchor;
+	unsigned long long _code; // usually used as a bitmap
+
+	lex_node(kuroda::parser<lexeme>::sequence& dest, size_t lb, size_t ub, unsigned long long code);	// slicing constructor
+
+public:
+	lex_node() noexcept : _code(0) {}
+	// \todo anchor constructor
+	lex_node(const lex_node& src) = delete;
+	lex_node(lex_node&& src) = default;
+	lex_node& operator=(const lex_node& src) = delete;
+	lex_node& operator=(lex_node&& src) = default;
+	virtual ~lex_node() = default;
+
+	// factory function: slices a lex_node out of dest, then puts the lex_node at index lb
+	void slice(kuroda::parser<lexeme>::sequence& dest, size_t lb, size_t ub, unsigned long long code = 0);
+
+	formal::src_location origin() const;
+
+	auto code() const { return _code; }
+	void interpret(unsigned long long src) { _code = src; }
+
+	std::string to_s() const;
+
+private:
+	static formal::src_location origin(const lexeme& src);
+	std::string to_s(formal::src_location& track) const;
+	static std::string to_s(const lexeme& src, formal::src_location& track);
+	static std::string to_s(const kuroda::parser<lexeme>::sequence& src, formal::src_location& track);
+};
+
+// we only handle closed intervals
+lex_node::lex_node(kuroda::parser<lexeme>::sequence& dest, size_t lb, size_t ub, unsigned long long code)
+: _code(code)
+{
+	assert(lb < ub);
+	assert(dest.size() > ub);
+
+	const size_t delta = ub - lb;
+	if (2 <= delta) {
+		decltype(_infix) staging(delta - 1);
+		memmove(staging.c_array(), dest.data() + lb + 1, sizeof(lexeme*) * (delta - 1));
+		std::fill_n(dest.c_array() + lb + 1, delta - 1, nullptr);
+		staging.swap(_infix);
+	}
+	_anchor.reset(dest[lb]);
+	_post_anchor.reset(dest[ub]);
+	dest[lb] = nullptr;
+	dest[ub] = nullptr;
+}
+
+void lex_node::slice(kuroda::parser<lexeme>::sequence& dest, size_t lb, size_t ub, unsigned long long code)
+{
+	assert(lb < ub);
+	assert(dest.size() > ub);
+
+	const auto audit = dest.size();	// remove before commit
+	const size_t delta = ub - lb;
+
+	std::unique_ptr<lexeme> stage(new lexeme(lex_node(dest, lb, ub, code)));
+	dest[lb] = stage.release();
+	dest.DeleteNSlotsAt(delta, lb + 1);
+	SUCCEED_OR_DIE(audit == dest.size() + delta);
+}
+
+formal::src_location lex_node::origin() const
+{
+	if (!_prefix.empty()) return origin(*_prefix.front());
+	if (_anchor) return origin(*_anchor);
+	if (!_infix.empty()) return origin(*_infix.front());
+	if (_post_anchor) return origin(*_post_anchor);
+	if (!_postfix.empty()) return origin(*_postfix.front());
+	return formal::src_location();
+}
+
+formal::src_location lex_node::origin(const lexeme& src)
+{
+	if (auto x = std::get_if<formal::word>(&src)) return x->origin();
+	return std::get<lex_node>(src).origin();
+}
+
+std::string lex_node::to_s() const
+{
+	auto track = origin();
+	return to_s(track);
+}
+
+std::string lex_node::to_s(formal::src_location& track) const
+{
+	std::string ret;
+
+	if (!_prefix.empty()) ret += to_s(_prefix, track);
+	if (_anchor) ret += to_s(*_anchor, track);
+	if (!_infix.empty()) ret += to_s(_infix, track);
+	if (_post_anchor) ret += to_s(*_post_anchor, track);
+	if (!_postfix.empty()) ret += to_s(_postfix, track);
+
+	return ret;
+}
+
+std::string lex_node::to_s(const lexeme& src, formal::src_location& track)
+{
+	std::string ret;
+	const auto start = origin(src);
+	if (start.line_pos.first != track.line_pos.first) {
+		// new line.  \todo Ignore indentation for one-line comments, but not normal source code
+		ret += '\n';
+	} else if (start.line_pos.first > track.line_pos.first) {
+		// need whitespace to look like original code
+		ret += std::string(start.line_pos.first- track.line_pos.first, ' ');
+	}
+
+	if (auto x = std::get_if<formal::word>(&src)) {
+		ret += std::string(x->value());
+		track = x->after();
+	} else ret += std::get<lex_node>(src).to_s(track);
+
+	return ret;
+}
+
+std::string lex_node::to_s(const kuroda::parser<lexeme>::sequence& src, formal::src_location& track)
+{
+	std::string ret;
+	for (decltype(auto) x : src) ret += to_s(*x, track);
+	return ret;
+}
+
 // lexing+preprocessing stage
 // main language syntax
 
@@ -175,6 +312,8 @@ int main(int argc, char* argv[], char* envp[])
 	decltype(auto) who_am_i = self_path(argv[0]);
 //	std::wcout << who_am_i.native() << "\n";
 //	std::wcout << std::filesystem::canonical(who_am_i.native()) << "\n";
+
+	lex_node test;
 
 	if (2 > argc) help();
 
